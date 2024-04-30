62791c87f87d5fea84af359b830f46e9a086b82c
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-04-30 17:18:48 +1000

    Update links.ts

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -17,6 +17,4 @@
 * along with SudoBot. If not, see <https://www.gnu.org/licenses/>.
 */
 
-export const WEBSITE_URL = "https://www.sudobot.org";
-export const DOCS_URL = "https://docs.sudobot.org";
-export const GITHUB_URL = "https://github.com/onesoft-sudo/sudobot";
+export const WEBSITE_URL = "https://bssr.xyz/";
