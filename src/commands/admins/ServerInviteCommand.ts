f9a3ac97aaf11b083156ee01f0bfb6c8d4b52240
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-05-03 18:19:14 +1000

    Update ServerInviteCommand.ts

--- src/commands/admins/ServerInviteCommand.ts
+++ src/commands/admins/ServerInviteCommand.ts
@@ -1,35 +1,37 @@
-import { ChatInputCommandInteraction, TextChannel, ChannelType } from "discord.js";
-import Command, { type CommandReturn } from "../../core/Command";
+import { ChatInputCommandInteraction, Message, TextChannel, ChannelType } from "discord.js";
+import Command, { type CommandReturn, type BasicCommandContext } from "../../core/Command";
 
 export default class ServerInviteCommand extends Command {
     public readonly name = "serverinvite";
     public readonly systemAdminOnly = true;
     public readonly description = "Get an invite link to a server by ID.";
 
-    public readonly supportsLegacy = false;
+    async execute(message: Message | ChatInputCommandInteraction, context: BasicCommandContext): Promise<CommandReturn> {
+        await this.deferIfInteraction(message);
 
-    async execute(interaction: ChatInputCommandInteraction): Promise<CommandReturn> {
-        await interaction.deferReply();
+        if (context.isLegacy && !context.args[0]) {
+            return void this.error(message, "You must specify a guild ID!");
+        }
 
-        const guildId = interaction.options.getString("guildId", true);
+        const guildId = context.isLegacy ? context.args[0] : context.options.getString("guildId", true);
         const guild = this.client.guilds.cache.get(guildId);
 
         if (!guild) {
-            await this.error(interaction, "Invalid guild ID provided.");
+            await this.error(message, "Invalid guild ID provided.");
             return;
         }
 
         const systemChannel = guild.systemChannel || guild.channels.cache.find(ch => ch.type === ChannelType.GuildText) as TextChannel;
         if (!systemChannel) {
-            await this.error(interaction, "Unable to find a suitable channel to create an invite.");
+            await this.error(message, "Unable to find a suitable channel to create an invite.");
             return;
         }
 
         try {
             const invite = await systemChannel.createInvite({ unique: true });
-            await this.success(interaction, `Here is the invite to the server: ${invite.url}`);
+            await this.success(message, `Here is the invite to the server: ${invite.url}`);
         } catch (error) {
-            await this.error(interaction, "Failed to create an invite to the server.");
+            await this.error(message, "Failed to create an invite to the server.");
         }
     }
 }
