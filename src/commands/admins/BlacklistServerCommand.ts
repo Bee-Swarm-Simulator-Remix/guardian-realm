36ee925066ca9aebb278b05d958a2da6269b836f
nin <95270582+ninsaccplayzyt@users.noreply.github.com>
2024-05-10 16:42:55 +1000

    Update BlacklistServerCommand.ts

--- src/commands/admins/BlacklistServerCommand.ts
+++ src/commands/admins/BlacklistServerCommand.ts
@@ -1,25 +1,27 @@
-import { Message | ChatInputCommandInteraction } from "discord.js";
+import { Message, ChatInputCommandInteraction } from "discord.js";
 import Command, { CommandReturn, AnyCommandContext } from "../../core/Command";
+
+// Import the blacklistedGuilds set from the file where it's defined
+import { blacklistedGuilds } from "./path/to/blacklistedGuildsFile";
 
 export default class BlacklistServerCommand extends Command {
     public readonly name = "blacklistserver";
     public readonly systemAdminOnly = true;
-    public readonly description = "Blacklist a server by ID.";
+    public readonly description = "Blacklist a server from getting invite links.";
 
-    const blacklistedGuilds: Set<string> = new Set();
-
-    async execute(message: CommandMessage, context: AnyCommandContext): Promise<CommandReturn> {
+    async execute(message: Message | ChatInputCommandInteraction, context: AnyCommandContext): Promise<CommandReturn> {
         await this.deferIfInteraction(message);
 
-        if (context.isLegacy && !context.args[0]) {
+        if (!context.args[0] && !context.options.getString("guildId", true)) {
             return void this.error(message, "You must specify a guild ID!");
         }
 
         const guildId = context.isLegacy ? context.args[0] : context.options.getString("guildId", true);
 
-        // Add the guild ID to the blacklist
-        blacklistedGuilds.add(guildId);
+        // Add the guild ID to the blacklistedGuilds set
+        this.blacklistedGuilds.add(guildId);
 
+        // Respond with a success message
         await this.success(message, `Server with ID ${guildId} has been blacklisted.`);
     }
 }
